7a6d0d42595a956e27dd0ecdac86568b92f595cf
kiel <khwood0912@gmail.com>
2019-12-14 13:38:29 -0500

    removed unnecessary colon

--- initialize.ps1
+++ initialize.ps1
@@ -1,4 +1,4 @@
 $loc = Get-Location
-$projName = Read-Host 'Enter the new project name:'
+$projName = Read-Host 'Enter the new project name'
 Start-Process -FilePath "etc/ProjectInitializer/TemplateConfig.exe" -ArgumentList $projName, $loc
 
